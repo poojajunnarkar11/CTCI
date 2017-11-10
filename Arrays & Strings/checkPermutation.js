1dc64c231653a9a78aa91047342d6d8748651044
Pooja Junnarkar <pooja.junnarkar@copart.com>
2017-11-10 17:02:28 -0600

    assumptions

--- Arrays & Strings/checkPermutation.js
+++ Arrays & Strings/checkPermutation.js
@@ -1,5 +1,6 @@
+//checking case sensitive permutation and considering whitespace
 const checkPermutation = (string1, string2) => {
   return string1.split('').sort().join('') === string2.split('').sort().join('')
 }
 
-console.log(checkPermutation('onion', 'ononi'))
+console.log(checkPermutation('onion', 'oNOni'))
